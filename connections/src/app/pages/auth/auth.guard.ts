4c2ec3e1cf637944f4bef590ecfaf750e80cb4c2
NMakarevich <nikoladze529@mail.ru>
2023-12-09 10:21:38 +0300

    fix: fix navigation to auth pages if user is authorized

--- connections/src/app/pages/auth/auth.guard.ts
+++ connections/src/app/pages/auth/auth.guard.ts
@@ -2,8 +2,11 @@ import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { TOKEN } from '../../utils/consts';
 
-export const authGuard: CanActivateFn = () => {
-  return localStorage.getItem(TOKEN)
-    ? true
-    : inject(Router).createUrlTree(['signin']);
+export const authGuard: CanActivateFn = (route) => {
+  const isAuth = localStorage.getItem(TOKEN);
+  const authURLs = ['signin', 'signup'];
+  const [urlSegment] = route.url;
+  if (isAuth && authURLs.includes(urlSegment?.path))
+    inject(Router).navigate(['']);
+  return isAuth ? true : inject(Router).createUrlTree(['signin']);
 };
